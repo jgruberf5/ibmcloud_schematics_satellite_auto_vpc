c31752adc68655f2d7dad82cb53e305910142a04
John Gruber <j.gruber@f5.com>
2022-02-06 13:07:26 -0600

    added random string to location name to speed up testing

--- modules/satellite_location/main.tf
+++ modules/satellite_location/main.tf
@@ -1,5 +1,9 @@
+resource "random_string" "location_unique" {
+  length  = 8
+  special = false
+}
 resource "ibm_satellite_location" "location" {
-  location          = "${var.ibm_resource_prefix}-location"
+  location          = "${var.ibm_resource_prefix}-location-${random_string.location_unique}"
   zones             = ["${var.ibm_region}-1", "${var.ibm_region}-2", "${var.ibm_region}-3"]
   managed_from      = var.ibm_location_managed_from
   resource_group_id = data.ibm_resource_group.group.id
